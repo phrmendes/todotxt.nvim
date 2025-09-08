e8986aec09399e1833a4e6664626e842764a80dd
Edmund Miller <git@edmundmiller.dev>
2025-09-07 21:30:11 -0500

    feat: update project patterns for hierarchical support
    - Add support for dash separators in project names (+project-sub)
    - Add new pattern variants for parsing hierarchical projects
    - Reference: https://github.com/todotxt/todo.txt-cli/issues/454
    - Backward compatible with existing flat project syntax

--- lua/todotxt/patterns.lua
+++ lua/todotxt/patterns.lua
@@ -13,5 +13,9 @@ return {
 	priority = "%(%a%)",
 	priority_letter = "%((%a)%)",
 	priority_with_space = "%(%a%)%s+",
-	project = "%+%w+",
+	-- Hierarchical project patterns (see: https://github.com/todotxt/todo.txt-cli/issues/454)
+	project = "%+[%w%-]+", -- Updated to support dashes: +project-subproject
+	project_full = "%+[%w%-]+", -- Full hierarchical project pattern
+	project_parent = "%+([%w]+)", -- Captures parent project name (text before first dash)
+	project_hierarchy = "%+([%w%-]+)", -- Captures full project hierarchy
 }
