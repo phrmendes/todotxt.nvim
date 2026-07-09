4dfc77bbdc790f078d0d89a2f4a4d955584a73ea
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-09 12:14:55 -0300

    chore: update devenv

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   name = "todotxt";
-
-  packages = with pkgs; [ neovim ];
 
   tasks = {
     "test:run".exec = ''nvim --headless --noplugin -u ./scripts/init.lua -c "lua MiniTest.run()"'';
